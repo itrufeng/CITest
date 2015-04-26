9b325093d1f0e08d4123f9cab17cd13cd2e9c637
Troy <jizhang@thoughtworks.com>
2015-04-26 16:16:15 +0800

    Initial Commit

---
+++ KiwiTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KiwiTest
+//
+//  Created by Jian Zhang on 4/26/15.
+//  Copyright (c) 2015 Steve (Zhang Jian). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KiwiTestTests/KiwiTestTests.m
@@ -0,0 +1,40 @@
+//
+//  KiwiTestTests.m
+//  KiwiTestTests
+//
+//  Created by Jian Zhang on 4/26/15.
+//  Copyright (c) 2015 Steve (Zhang Jian). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface KiwiTestTests : XCTestCase
+
+@end
+
+@implementation KiwiTestTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
